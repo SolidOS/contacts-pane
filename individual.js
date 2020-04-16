ade2be0f3813eecbd707015f407a270516f32e9b
Tim Berners-Lee <timbl+git@w3.org>
2020-04-16 22:13:32 +0100

    Split off inidividual view and mugshot gallery into sep files

---
+++ individual.js
@@ -0,0 +1,207 @@
+import * as UI from 'solid-ui'
+import { renderMugshotGallery } from './mugshotGallery'
+const $rdf = UI.rdf
+const ns = UI.ns
+const utils = UI.utils
+const kb = UI.store
+
+// Render Individual card
+
+export function renderIndividual (dom, div, subject) {
+  // ////////////////////  DRAG and Drop for mugshot image
+
+  function complain (message) {
+    console.log(message)
+    div.appendChild(UI.widgets.errorMessageBlock(dom, message, 'pink'))
+  }
+
+  function complainIfBad (ok, body) {
+    if (!ok) {
+      complain('Error: ' + body)
+    }
+  }
+
+  /// ///////////////////////////
+
+  div.appendChild(renderMugshotGallery(dom, subject))
+
+  // Background metadata for this pane we bundle with the JS
+  var individualForm = kb.sym(
+    'https://solid.github.io/solid-panes/contact/individualForm.ttl#form1'
+  )
+  var individualFormDoc = individualForm.doc()
+  if (!kb.holds(undefined, undefined, undefined, individualFormDoc)) {
+    // If not loaded already
+    var individualFormText = require('./individualForm.js')
+    $rdf.parse(individualFormText, kb, individualFormDoc.uri, 'text/turtle') // Load form directly
+  }
+  var vcardOnt = UI.ns.vcard('Type').doc()
+  if (!kb.holds(undefined, undefined, undefined, vcardOnt)) {
+    // If not loaded already
+    $rdf.parse(require('./vcard.js'), kb, vcardOnt.uri, 'text/turtle') // Load ontology directly
+  }
+
+  var toBeFetched = [subject.doc()] // was: individualFormDoc, UI.ns.vcard('Type').doc()
+  kb.fetcher
+    .load(toBeFetched)
+
+    .catch(function (e) {
+      console.log('Error: Failed to load subject: ' + e)
+    }) // load.then
+
+    .then(function (_xhrs) {
+      function setPaneStyle () {
+        var types = kb.findTypeURIs(subject)
+        var mystyle = 'padding: 0.5em 1.5em 1em 1.5em; '
+        var backgroundColor = null
+        for (var uri in types) {
+          backgroundColor = kb.anyValue(
+            kb.sym(uri),
+            ns.solid('profileHighlightColor')
+          )
+          if (backgroundColor) break
+        }
+        // allow the parent element to define background by default
+        backgroundColor = backgroundColor || 'transparent'
+        mystyle += 'background-color: ' + backgroundColor + '; '
+        div.setAttribute('style', mystyle)
+      }
+      setPaneStyle()
+
+      UI.authn.checkUser() // kick off async operation
+
+      div.appendChild(renderMugshotGallery(subject))
+
+      UI.widgets.appendForm(
+        dom,
+        div,
+        {},
+        subject,
+        individualForm,
+        subject.doc(),
+        complainIfBad
+      )
+
+      div
+        .appendChild(dom.createElement('tr'))
+        .setAttribute('style', 'height: 1em') // spacer
+
+      function lookUpId (dom, container, x) {
+        var tr = table.appendChild(dom.createElement('tr'))
+        tr.setAttribute('style', 'margin-top: 0.1em solid #ccc;')
+        var nameTD = tr.appendChild(dom.createElement('td'))
+        var formTD = tr.appendChild(dom.createElement('td'))
+        nameTD.textContent = x.uri.split('/')[2]
+
+        kb.fetcher
+          .load(x)
+          .then(function (_xhr) {
+            nameTD.textContent =
+              x.uri.split('/')[2] +
+              ' (' +
+              kb.statementsMatching(
+                undefined,
+                undefined,
+                undefined,
+                x.doc()
+              ).length +
+              ')'
+          })
+          .catch(function (e) {
+            formTD.appendChild(UI.widgets.errorMessageBlock(dom, e, 'pink'))
+          })
+
+        formTD.appendChild(UI.widgets.linkIcon(dom, x))
+      }
+
+      var table = div.appendChild(dom.createElement('table'))
+
+      var aliases = kb.allAliases(subject)
+      if (aliases.length > 1) {
+        for (var i = 0; i < aliases.length; i++) {
+          var x = aliases[i]
+          if (!x.sameTerm(subject)) {
+            lookUpId(dom, table, x)
+            // UI.widgets.appendForm(dom, formTD, {}, x, individualForm, x.doc(), complainIfBad)
+          }
+        }
+      }
+
+      UI.widgets.attachmentList(dom, subject, div, {
+        // promptIcon: UI.icons.iconBase +  'noun_681601.svg',
+        predicate: UI.ns.vcard('url') // @@@@@@@@@ ,--- no, the vcard ontology structure uses a bnode.
+      })
+
+      div.appendChild(dom.createElement('hr'))
+
+      var pages = kb.each(subject, ns.vcard('url')) // vcard:url [ a vcard:HomePage; vcard:value <http://www.w3.org/People/Berners-Lee>],
+      pages.forEach(function (p) {
+        var cla = kb.any(p, ns.rdf('type'))
+        var val = kb.any(p, ns.vcard('value'))
+        if (val) {
+          var tr = table.appendChild(dom.createElement('tr'))
+          tr.setAttribute('style', 'margin-top: 0.1em solid #ccc;')
+
+          var nameTD = tr.appendChild(dom.createElement('td'))
+          nameTD.textContent = utils.label(cla)
+
+          var formTD = tr.appendChild(dom.createElement('td'))
+          var anchor = formTD.appendChild(dom.createElement('a'))
+          anchor.setAttribute('href', val.uri)
+          var span = anchor.appendChild(dom.createElement('span'))
+          span.textContent = val.uri
+        }
+      })
+
+      div.appendChild(dom.createElement('hr'))
+
+      // Remove a person from a group
+
+      function removeFromGroup (thing, group) {
+        var pname = kb.any(thing, ns.vcard('fn'))
+        var gname = kb.any(group, ns.vcard('fn'))
+        var groups = kb.each(null, ns.vcard('hasMember'), thing)
+        if (groups.length < 2) {
+          alert(
+            'Must be a member of at least one group.  Add to another group first.'
+          )
+          return
+        }
+        var message = 'Remove ' + pname + ' from group ' + gname + '?'
+        if (confirm(message)) {
+          var del = [
+            $rdf.st(group, ns.vcard('hasMember'), thing, group.doc()),
+            $rdf.st(thing, ns.vcard('fn'), pname, group.doc())
+          ]
+          kb.updater.update(del, [], function (uri, ok, err) {
+            if (!ok) {
+              return complain(
+                'Error removing member from group ' + group + ': ' + err
+              )
+            }
+            console.log('Removed ' + pname + ' from group ' + gname)
+            syncGroupList()
+          })
+        }
+      }
+
+      function newRowForGroup (group) {
+        var options = {
+          deleteFunction: function () {
+            removeFromGroup(subject, group)
+          },
+          noun: 'membership'
+        }
+        var tr = UI.widgets.personTR(dom, null, group, options)
+        return tr
+      }
+
+      var groupList = div.appendChild(dom.createElement('table'))
+      function syncGroupList () {
+        var groups = kb.each(null, ns.vcard('hasMember'), subject)
+        utils.syncTableToArray(groupList, groups, newRowForGroup)
+      }
+      groupList.refresh = syncGroupList
+      syncGroupList()
+    })
+} // renderIndividual
---
+++ mugshotGallery.js
@@ -0,0 +1,296 @@
+import * as UI from 'solid-ui'
+import * as mime from 'mime-types'
+
+const $rdf = UI.rdf
+const ns = UI.ns
+const utils = UI.utils
+const kb = UI.store
+
+// //////// End of drag and drop
+
+export function renderMugshotGallery (dom, subject) {
+  function complain (message) {
+    console.log(message)
+    galleryDiv.appendChild(UI.widgets.errorMessageBlock(dom, message, 'pink'))
+  }
+
+  async function linkToPicture (subject, pic, remove) {
+    const link = [
+      $rdf.st(subject, ns.vcard('hasPhoto'), pic, subject.doc())
+    ]
+    try {
+      if (remove) {
+        await kb.updater.update(link, [])
+      } else {
+        await kb.updater.update([], link)
+      }
+    } catch (err) {
+      const msg = ' Write back image link FAIL ' + pic + ', Error: ' + err
+      console.log(msg)
+      alert(msg)
+    }
+  }
+
+  function handleDroppedThing (thing) {
+    kb.fetcher.nowOrWhenFetched(thing.doc(), function (ok, mess) {
+      if (!ok) {
+        console.log('Error looking up dropped thing ' + thing + ': ' + mess)
+      } else {
+        var types = kb.findTypeURIs(thing)
+        for (var ty in types) {
+          console.log('    drop object type includes: ' + ty) // @@ Allow email addresses and phone numbers to be dropped?
+        }
+        console.log('Default: assume web page  ' + thing) // icon was: UI.icons.iconBase + 'noun_25830.svg'
+        kb.add(subject, ns.wf('attachment'), thing, subject.doc())
+        // @@ refresh UI
+      }
+    })
+  }
+
+  function uploadFileToContact (filename, contentType, data) {
+    // var fileExtension = filename.split('.').pop() // .toLowerCase()
+    var extension = mime.extension(contentType)
+    if (contentType !== mime.lookup(filename)) {
+      filename += '_.' + extension
+      console.log('MIME TYPE MISMATCH -- adding extension: ' + filename)
+    }
+    let prefix, predicate
+    const isImage = contentType.startsWith('image')
+    if (isImage) {
+      prefix = 'image_'
+      predicate = ns.vcard('hasPhoto')
+    } else {
+      prefix = 'attachment_'
+      predicate = ns.wf('attachment')
+    }
+
+    var n, pic
+    for (n = 0; ; n++) {
+      // Check filename is not used or invent new one
+      pic = kb.sym(subject.dir().uri + filename)
+      if (!kb.holds(subject, ns.vcard('hasPhoto'), pic)) {
+        break
+      }
+      filename = prefix + n + '.' + extension
+    }
+    console.log(
+      'Putting ' +
+        data.byteLength +
+        ' bytes of ' +
+        contentType +
+        ' to ' +
+        pic
+    )
+    kb.fetcher
+      .webOperation('PUT', pic.uri, {
+        data: data,
+        contentType: contentType
+      })
+      .then(function (response) {
+        if (!response.ok) {
+          complain('Error uploading ' + pic + ':' + response.status)
+          return
+        }
+        console.log(' Upload: put OK: ' + pic)
+        kb.add(subject, predicate, pic, subject.doc())
+        kb.fetcher
+          .putBack(subject.doc(), { contentType: 'text/turtle' })
+          .then(
+            function (_response) {
+              if (isImage) {
+                mugshotDiv.refresh()
+              }
+            },
+            function (err) {
+              console.log(
+                ' Write back image link FAIL ' + pic + ', Error: ' + err
+              )
+            }
+          )
+      })
+  }
+
+  // When a set of URIs are dropped on
+  function handleURIsDroppedOnMugshot (uris) {
+    uris.map(function (u) {
+      var thing = $rdf.sym(u) // Attachment needs text label to disinguish I think not icon.
+      console.log('Dropped on mugshot thing ' + thing) // icon was: UI.icons.iconBase + 'noun_25830.svg'
+      if (u.startsWith('http') && u.indexOf('#') < 0) {
+        // Plain document
+        // Take a copy of a photo on the web:
+        const options = { withCredentials: false, credentials: 'omit' }
+        kb.fetcher.webOperation('GET', thing.uri, options).then(
+          result => {
+            const contentType = result.headers.get('Content-Type')
+            // let data = result.responseText
+            let pathEnd = thing.uri.split('/').slice(-1)[0] // last segment as putative filename
+            pathEnd = pathEnd.split('?')[0] // chop off any query params
+            result.arrayBuffer().then(function (data) {
+              // read text stream
+              if (!result.ok) {
+                complain('Error downloading ' + thing + ':' + result.status)
+                return
+              }
+              uploadFileToContact(pathEnd, contentType, data)
+            })
+          },
+          err => {
+            complain(
+              `WebOp (fetch) error trying to read picture ${thing} on web: ${err}`
+            )
+          }
+        )
+        return
+      } else {
+        console.log(
+          'Not a web document URI, cannot copy as picture: ' + thing
+        )
+      }
+      handleDroppedThing(thing)
+    })
+  }
+
+  // Drop an image file to set up the mugshot
+  function droppedFileHandler (files) {
+    for (var i = 0; i < files.length; i++) {
+      const f = files[i]
+      console.log(
+        ' contacts: Filename: ' +
+          f.name +
+          ', type: ' +
+          (f.type || 'n/a') +
+          ' size: ' +
+          f.size +
+          ' bytes, last modified: ' +
+          (f.lastModifiedDate
+            ? f.lastModifiedDate.toLocaleDateString()
+            : 'n/a')
+      ) // See e.g. https://www.html5rocks.com/en/tutorials/file/dndfiles/
+
+      // @@ Add: progress bar(s)
+      var reader = new FileReader()
+      reader.onload = (function (theFile) {
+        return function (e) {
+          var data = e.target.result
+          console.log(' File read byteLength : ' + data.byteLength)
+          var filename = encodeURIComponent(theFile.name)
+          var contentType = theFile.type
+          uploadFileToContact(filename, contentType, data)
+        }
+      })(f)
+      reader.readAsArrayBuffer(f)
+    }
+  }
+
+  function elementForImage (image) {
+    const img = dom.createElement('img')
+    img.setAttribute(
+      'style',
+      'max-height: 10em; border-radius: 1em; margin: 0.7em;'
+    )
+    UI.widgets.makeDropTarget(
+      img,
+      handleURIsDroppedOnMugshot,
+      droppedFileHandler
+    )
+    if (image) {
+      img.setAttribute('src', image.uri)
+      UI.widgets.makeDraggable(img, image)
+    }
+    return img
+  }
+
+  function syncMugshots () {
+    let images = kb.each(subject, ns.vcard('hasPhoto')) // Priviledge vcard ones
+    images.sort() // arbitrary consistency
+    images = images.slice(0, 5) // max number for the space
+    if (images.length === 0) {
+      mugshotDiv.innerHTML = '' // strictly, don't remove it if already there
+      mugshotDiv.appendChild(placeholder)
+    } else {
+      utils.syncTableToArray(mugshotDiv, images, elementForImage)
+    }
+  }
+
+  // Good URI for a Camera picture
+  function getImageDoc () {
+    const imageDoc = kb.sym(
+      subject.dir().uri + 'Image_' + Date.now() + '.png'
+    )
+    return imageDoc
+  }
+  // Store picture
+  async function tookPicture (imageDoc) {
+    if (imageDoc) {
+      await linkToPicture(subject, imageDoc)
+      syncMugshots()
+    }
+  }
+
+  function trashCan () {
+    const button = UI.widgets.button(
+      dom,
+      UI.icons.iconBase + 'noun_925021.svg',
+      'Drag here to delete'
+    )
+    async function droppedURIHandler (uris) {
+      const images = kb
+        .each(subject, ns.vcard('hasPhoto'))
+        .map(x => x.uri)
+      for (var uri of uris) {
+        if (!images.includes(uri)) {
+          alert('Only drop images in this contact onto this trash can.')
+          return
+        }
+        if (confirm(`Permanently DELETE image ${uri} completely?`)) {
+          console.log('Unlinking image file ' + uri)
+          await linkToPicture(subject, kb.sym(uri), true)
+          try {
+            console.log('Deleting image file ' + uri)
+            await kb.fetcher.webOperation('DELETE', uri)
+          } catch (err) {
+            alert('Unable to delete picture! ' + err)
+          }
+        }
+      }
+      syncMugshots()
+    }
+    UI.widgets.makeDropTarget(button, droppedURIHandler, null)
+    return button
+  }
+
+  function renderImageTools () {
+    const imageToolTable = dom.createElement('table')
+    const row = imageToolTable.appendChild(dom.createElement('tr'))
+    const left = row.appendChild(dom.createElement('td'))
+    const middle = row.appendChild(dom.createElement('td'))
+    const right = row.appendChild(dom.createElement('td'))
+
+    left.appendChild(
+      UI.media.cameraButton(dom, kb, getImageDoc, tookPicture)
+    ) // 20190812
+    try {
+      middle.appendChild(
+        UI.widgets.fileUploadButtonDiv(dom, droppedFileHandler)
+      )
+    } catch (e) {
+      console.log('ignore fileUploadButtonDiv error for now', e)
+    }
+    right.appendChild(trashCan())
+    return imageToolTable
+  }
+
+  // Body of renderMugshotGallery
+
+  const editable = kb.updater.editable(subject.doc().uri, kb) // @@ ToDo -- also check wac-allow
+  const galleryDiv = dom.createElement('div')
+  const mugshotDiv = galleryDiv.appendChild(dom.createElement('div'))
+  var placeholder = elementForImage()
+  UI.widgets.setImage(placeholder, subject) // Fallback icon or get from web
+  syncMugshots()
+  mugshotDiv.refresh = syncMugshots
+  if (editable) {
+    galleryDiv.appendChild(renderImageTools())
+  }
+  return galleryDiv
+}
