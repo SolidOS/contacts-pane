1d36140bf5591b2abea70b7456d9b85d1693f130
Tim Berners-Lee <timbl+git@w3.org>
2021-05-12 12:57:41 +0100

    pull in publicData from solid-ui for now, may change

--- webidControl.js
+++ webidControl.js
@@ -1,9 +1,6 @@
 // Render a control to record the webids we have for this agent
 /* eslint-disable multiline-ternary */
 import * as UI from 'solid-ui'
-// import { renderAutoComplete } from './lib/autocompletePicker' // dbpediaParameters
-import { renderAutocompleteControl } from './lib/autocompleteBar'
-import { wikidataParameters, loadPublicDataThing, wikidataClasses } from './lib/publicData' // dbpediaParameters
 
 const $rdf = UI.rdf
 const ns = UI.ns
@@ -11,6 +8,9 @@ const widgets = UI.widgets
 const utils = UI.utils
 const kb = UI.store
 const style = UI.style
+
+const wikidataClasses = widgets.publicData.wikidataClasses // @@ move to solid-logic
+const wikidataParameters = widgets.publicData.wikidataParameters // @@ move to solid-logic
 
 const WEBID_NOUN = 'Solid ID'
 const PUBLICID_NOUN = 'In public data'
@@ -215,7 +215,7 @@ export async function renderIdControl (person, dataBrowserContext, options) {
     delete openButton.style.border
     const paneName = isOrganization(person) || isOrganization(persona) ? 'profile' : 'profile' // was default for org
 
-    loadPublicDataThing(kb, person, persona).then(_resp => {
+    widgets.publicData.loadPublicDataThing(kb, person, persona).then(_resp => {
       try {
         main = renderNamedPane(dom, persona, paneName, dataBrowserContext)
         console.log('main: ', main)
@@ -273,7 +273,7 @@ export async function renderIdControl (person, dataBrowserContext, options) {
 
   if (options.editable) { // test
     options.queryParams = options.queryParams || wikidataParameters
-    div.appendChild(await renderAutocompleteControl(dom, person, options, addOneIdAndRefresh))
+    div.appendChild(await widgets.renderAutocompleteControl(dom, person, options, addOneIdAndRefresh))
   }
   const profileArea = div.appendChild(dom.createElement('div'))
   await refreshWebIDTable()
