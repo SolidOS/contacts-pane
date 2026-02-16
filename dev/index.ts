39b843233bc9495af99cc914fcc80b96323dab97
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-02-16 10:28:12 +0100

    local dev setup

---
+++ dev/index.ts
@@ -0,0 +1,31 @@
+import { sym } from 'rdflib'
+import { default as pane } from '../src/contactsPane' 
+import './dev-global.css' // Import after src to override component styles
+import { context, fetcher } from './context'
+import { authn, authSession } from 'solid-logic'
+import * as UI from 'solid-ui'
+
+const loginBanner = document.getElementById('loginBanner')
+const webId = document.getElementById('webId')
+
+loginBanner.appendChild(UI.login.loginStatusBox(document, null, {}))
+
+async function finishLogin() {
+  await authSession.handleIncomingRedirect()
+  const session = authSession
+  if (session.info.isLoggedIn) {
+    // Update the page with the status.
+    webId.innerHTML = 'Logged in as: ' + authn.currentUser().uri
+  } else {
+    webId.innerHTML = ''
+  }
+  fetcher.load(webIdToShow).then(() => {
+  const app = pane.render(sym(webIdToShow), context)
+  document.getElementById('app').replaceWith(app)
+})
+}
+
+finishLogin()
+
+const webIdToShow = 'https://testingsolidos.solidcommunity.net/profile/card#me'
+
