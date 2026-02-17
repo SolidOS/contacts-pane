08ab96a5a170c9b96f4ee9bdc055b1db0cc6e2da
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-02-17 09:34:25 +0100

    remove ts from webpack

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -10,11 +10,6 @@ const common = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
